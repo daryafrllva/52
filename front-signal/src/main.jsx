e9509345dcb4e4bca92668d4d90eb12c068cb8d8
kvintosis <kvitosis55887@mail.ru>
2025-04-25 22:31:31 +0700

    нихуя не поменялось

--- front-signal/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/styles.less'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
--- signal-front/src/App.jsx
+++ signal-front/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <Router>
-        <Header />
-    </Router>
+    <>
+    <Header />
+    <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   )
 }
 
