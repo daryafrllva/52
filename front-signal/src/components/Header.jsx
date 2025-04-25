ec95acae3fb70bbe11c771b89d7c935129c9b216
daryafrllva <dariyf995@gmail.com>
2025-04-25 21:06:17 +0700

    какие-то баги

---
+++ front-signal/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+function Header() {
+    return (
+        <Header className="block-header">
+            <div className="container_header">
+                <div className="container_header__logo">
+                    <p>здесь лого</p>
+                </div>
+
+            </div>
+        </Header>
+    )
+}
+
+export default Header;
